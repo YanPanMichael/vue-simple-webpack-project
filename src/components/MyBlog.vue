0535733ea768300516167c22c58b787b359d1efa
YanPanMichael <yanpancsu@icloud.com>
2018-07-02 00:52:39 +0800

    create initial empty vue webpack program

---
+++ src/components/MyBlog.vue
@@ -0,0 +1,34 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
+
+<template>
+  <div class="my-blog">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'myBlog',
+        data () {
+            return {
+                msg: 'Welcome to my blog'
+            }
+        }
+    }
+</script>
+
